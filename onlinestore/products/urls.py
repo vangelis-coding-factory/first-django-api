7dc6288ab2718a7803bed26962820cc0cb8a9c14
Vagelis88 <tsougiannisvag@gmail.com>
2023-11-04 10:09:36 +0200

    Initial commit

---
+++ onlinestore/products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+#from .views import ProductDetailView, ProductListView
+from .views import product_list, product_detail
+
+urlpatterns = [
+    path("products/", product_list, name="product-list"),
+    path("products/<int:pk>", product_detail, name="product-detail"),
+]
+
+""" urlpatterns = [
+    path("", ProductListView.as_view(), name="product-list"),
+    path("products/<int:pk>", ProductDetailView.as_view(), name="product-detail"),
+] """
---
+++ onlinestore/products/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.http import JsonResponse
+from .models import Manufacturer, Product
+
+# Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    data = {"products":list(products.values("pk","name"))}
+    response = JsonResponse(data)
+    return response
+
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        data = {"product":{
+            "name" : product.name,
+            "manufacturer" : product.manufacturer.name,
+            "description" : product.description,
+            "photo" : product.photo.url,
+            "price" : product.price,
+            "shipping_cost" : product.shipping_cost,
+            "quantity" : product.quantity
+        }}
+        response = JsonResponse(data)
+    except Product.DoesNotExist:
+        response = JsonResponse({
+            "error":{
+                "code":404,
+                "message":"product not found"
+            }
+        }, status=404)
+    
+    return response
+
+""" class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "products/product_list.html" """
